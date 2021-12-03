af385a6fb7d61c877a19c4408cec0f3657f8f016
edilson@Acer-E5-473 <ed.vilarinho@gmail.com>
2021-12-03 00:02:24 -0300

    Aula 04

--- App.js
+++ App.js
@@ -1,9 +1,24 @@
-import React, { Component } from 'react';
+//import React, { Component } from 'react';
+import React from 'react';
 import {Text,View} from 'react-native';
+import C1 from './componentes/comp1'
 
+// Componentes de função
+export default function App1(){
+  return (
+    <View>
+      <C1/>
+      <C1/>
+      <C1/>
+      <C1/>
+      <Text>youtube.com/cfbcursos</Text>      
+    </View>
+  );
+};
+ 
 
 // Componentes de classe
-export default class App1 extends Component{
+/* export default class App1 extends Component{
   render(){
     return(
       <View>
@@ -19,21 +34,5 @@ export default class App1 extends Component{
     );
   }
 }
-
-
-/* Componentes de função
-export default function App1(){
-  return (
-    <View>
-      <View>
-        <Text>CFB Cursos</Text>
-        <Text>Curso de React-Native</Text>
-      </View>
-      <View>
-        <Text>Aula 3</Text>
-        <Text>www.cfbcurso.com.br</Text>
-      </View>
-    </View>
-  );
-};
 */
+
---
+++ componentes/comp1.js
@@ -0,0 +1,8 @@
+import React from "react";
+import {Text} from 'react-native';
+
+export default function(){
+    return(
+        <Text>CFB Cursos - Curso de React Native</Text>
+    )
+}
