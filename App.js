419c054668d11772c82523b8bac077394a295591
edilson@Acer-E5-473 <ed.vilarinho@gmail.com>
2021-12-03 22:06:25 -0300

    Aula 05

--- App.js
+++ App.js
@@ -1,21 +1,34 @@
 //import React, { Component } from 'react';
 import React from 'react';
-import {Text,View} from 'react-native';
+import {StyleSheet,Text,View} from 'react-native';
 import C1 from './componentes/comp1'
 
 // Componentes de função
 export default function App1(){
   return (
-    <View>
+    <View style={estilos.conteiner}>
       <C1/>
       <C1/>
       <C1/>
       <C1/>
-      <Text>youtube.com/cfbcursos</Text>      
+      <Text style={estilos.txt0}>youtube.com/cfbcursos</Text>  
+      <Text style={{color: '#f00', fontSize: 30 }}>cfbcursos.com.br</Text>      
     </View>
   );
 };
- 
+
+const estilos=StyleSheet.create({
+  conteiner:{
+    flex: 1,
+    backgroundColor: '#444',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  txt0:{
+    color: '#080',
+    fontSize: 10,
+  }
+})
 
 // Componentes de classe
 /* export default class App1 extends Component{
